2801da39112ad2463fc9fe3941e43919d02bafdb
Func <Funcer@outlook.com>
2023-05-13 03:03:14 +0800

    Update util.sh

--- util.sh
+++ util.sh
@@ -23,22 +23,3 @@ function exists {
   else return 1
   fi  
 }
-
-function urlencode {
-  local length="${#1}"
-  for (( i = 0; i < length; i++ )); do
-    local c="${1:i:1}"
-    case $c in
-      [a-zA-Z0-9.~_-]) printf "$c" ;;
-      *) printf '%%%02X' "'$c"
-    esac
-  done
-}
-
-function urldecode {
-  local data
-  while read data; do
-    local url_encoded="${data//+/ }"
-    echo $(printf '%b' "${url_encoded//%/\x}")
-  done
-}
